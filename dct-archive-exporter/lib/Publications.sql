319298cd31af877ecb2fa981fd164275ebf7d9a9
MaxRS7 <1904985@uad.ac.uk>
2022-04-10 15:43:16 +0100

    Fixed SQL Syntax

--- dct-archive-exporter/lib/Publications.sql
+++ dct-archive-exporter/lib/Publications.sql
@@ -1,18 +1,10 @@
--- SQL file for creating and storing records data
+SET SQL_MODE = `NO_AUTO_VALUE_ON_ZERO`;
+SET time_zone = `+00:00`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
--- Table structure for table 'Records'
-
-CREATE TABLE IF NOT EXISTS 'Publications' (
-    'PubID' int(2) NOT NULL,
-    'Shortcode' varchar(3) NOT NULL,
-    'Name' varchar(40) NOT NULL
+CREATE TABLE IF NOT EXISTS `Publications` (
+    `PubID` int(2) NOT NULL,
+    `Shortcode` varchar(3) NOT NULL,
+    `Name` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---Dumping the data
-
-INSERT INTO 'Publications' ('PubID', 'Shortcode', 'Name') VALUES
---This is where the program loops over the data and inserts on lines (remember to add a line counter to increment after each write)
+INSERT INTO `Publications` (`PubID`, `Shortcode`, `Name`) VALUES
