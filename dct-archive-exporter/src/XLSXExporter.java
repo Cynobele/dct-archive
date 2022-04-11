2514dc7d5df299775b20f4234a436695ec4121bf
Cameron <cameron.harrison1@hotmail.co.uk>
2022-04-11 11:20:27 +0100

    Reformatted XLSXExporter class, now working

--- dct-archive-exporter/src/XLSXExporter.java
+++ dct-archive-exporter/src/XLSXExporter.java
@@ -1,77 +1,56 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Locale;
-
-import jxl.Workbook;
-import jxl.WorkbookSettings;
-import jxl.write.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XLSXExporter {
     
+    //SUMMARY
+    //table_data is the array imported from database
+    //file_path is the location selected by the user
+    //table is the selected table to export
     public void WriteFile(Object[][] table_data, String file_path, ExporterFrame.RADIO_BUTTON table){
         String file_name = "/" + table.toString() +"_DCTArchiveExport.xlsx";
         File file = new File(file_path + file_name);
         String[] photo_headings = {"id", "publication id", "date", "reference", "location", "caption"};
         String[] pub_headings = {"publication id", "shortcode", "name"};
         
-        try{    //attempt to create the doc
-            WorkbookSettings wbs = new WorkbookSettings();
-            wbs.setLocale(new Locale("en", "EN"));
-            WritableWorkbook workbook = Workbook.createWorkbook(file, wbs);
-            workbook.createSheet(table.toString(), 0); //create a blank sheet
-            WritableSheet sheet1 = workbook.getSheet(0); //make the sheet writable
-            Label label;
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet(table.toString());
+
+        if(table == ExporterFrame.RADIO_BUTTON.PHOTO){//write headings
+            Row row = sheet.createRow(0);
+            for(int i=0; i<photo_headings.length; i++){
+                Cell cell = row.createCell(i);
+                cell.setCellValue(photo_headings[i]);
+            }
+        }else{ //heading for publication table
+            Row row = sheet.createRow(0);
+            for(int i=0; i<pub_headings.length; i++){
+                Cell cell = row.createCell(i);
+                cell.setCellValue(pub_headings[i]);
+            }
+        }
+
+        try{ //attempt to export the table_data to xlsx file
             
-            //write the correct headings
-            if(table == ExporterFrame.RADIO_BUTTON.PHOTO){
-                for(int i=0; i<6; i++){
-                    label = new Label(i, 0, photo_headings[i]);
-                    sheet1.addCell(label);
-                }
-                workbook.createSheet(table.toString()+"1", 1);
-                WritableSheet sheet2 = workbook.getSheet(1);//create a 2nd worksheet as 1 is too small
-
-
-                //TODO : fix write exception
-                //When exporting the Photo table as XLSX, a write exception is thrown
-                //The maximum number of rows permitted on a worksheet been exceeded
-                //I am only writing 50k rows to one sheet, and the rest to the other
-                // but somehow still receiving the error...
-
-                for(int i=0; i<50000; i++){ //maximum number of rows on a sheet is 65536
-                    for(int j=0; j<table_data[i].length; j++){
-                        label = new Label(j, i+1, table_data[i][j].toString());
-                        sheet1.addCell(label);
-                    }
-                }
-                for(int i=50000; i<table_data.length; i++){
-                    for(int j=0; j<table_data[i].length; j++){
-                        label = new Label(j, i+1, table_data[i][j].toString());
-                        sheet2.addCell(label);
-                    }
-                }
-            }else if (table == ExporterFrame.RADIO_BUTTON.PUBLICATION){
-                for(int i=0; i<3; i++){
-                    label = new Label(i, 0, pub_headings[i]);
-                    sheet1.addCell(label);
-                }
-
-                for(int i=0; i<table_data.length; i++){
-                    for(int j=0; j<table_data[i].length; j++){                 //i=1, since i=0 would be the heading row
-                        label = new Label(j, i+1, table_data[i][j].toString()); //i-1 to make up for this
-                        sheet1.addCell(label);
-                    }
+            for(int i=0; i<table_data.length; i++){
+                Row row = sheet.createRow(i+1); //i+1 since i=0 is headings row
+                for(int j=0; j<table_data[i].length; j++){
+                    Cell cell = row.createCell(j);
+                    cell.setCellValue(table_data[i][j].toString());
                 }
             }
-            
-            workbook.write();
-            System.out.println("workbook exported successfully");
+
+            FileOutputStream output_stream = new FileOutputStream(file); //write to file
+            workbook.write(output_stream);
             workbook.close();
 
         }catch(IOException e){
-            System.out.println("XLSX IOException: " +e.getMessage());
-        }catch(WriteException we){
-            System.out.println("XLSX WriteException: " +we.getMessage());
+            System.out.println("XSLX Write exception:"+e.getMessage());
         }
     }
 }
